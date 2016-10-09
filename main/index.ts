7bed565729672b8f385c515a119adbd1d17c35de
rhysd <lin90162@yahoo.co.jp>
2016-10-09 17:15:22 +0900

    toggle window on hot key

--- main/index.ts
+++ main/index.ts
@@ -1,5 +1,6 @@
 import * as path from 'path';
 import * as menubar from 'menubar';
+import {globalShortcut} from 'electron';
 import loadConfig from './config';
 import log from './log';
 
@@ -7,7 +8,7 @@ process.on('unhandledRejection', (reason: string) => {
     log.error('FATAL: Unhandled rejection! Reason:', reason);
 });
 
-function setupMenuBar(_: Config) {
+function setupMenuBar(config: Config) {
     return new Promise<Menubar.MenubarApp>(resolve => {
         const html = `file://${path.join(__dirname, '..', 'renderer', 'index.html')}`;
         const mb = menubar({
@@ -15,6 +16,15 @@ function setupMenuBar(_: Config) {
         });
         mb.once('ready', () => {
             log.debug('Menubar application was launched:', mb);
+            if (config.hot_key) {
+                globalShortcut.register(config.hot_key, () => {
+                    if (mb.window.isFocused()) {
+                        mb.hideWindow();
+                    } else {
+                        mb.showWindow();
+                    }
+                });
+            }
             resolve(mb);
         });
     });
