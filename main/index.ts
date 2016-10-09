9adf69230a718e725c4ddee398ee9733c0cb5e93
rhysd <lin90162@yahoo.co.jp>
2016-10-09 17:49:32 +0900

    use my forked version 'menubar' to ensure to launch app

--- main/index.ts
+++ main/index.ts
@@ -11,19 +11,24 @@ process.on('unhandledRejection', (reason: string) => {
 function setupMenuBar(config: Config) {
     return new Promise<Menubar.MenubarApp>(resolve => {
         const html = `file://${path.join(__dirname, '..', 'renderer', 'index.html')}`;
+        log.debug('Will launch application:', html);
         const mb = menubar({
             index: html,
         });
-        mb.once('ready', () => {
-            log.debug('Menubar application was launched:', mb);
+        mb.once('ready', () => mb.showWindow());
+        mb.once('after-create-window', () => {
+            log.debug('Menubar application was launched');
             if (config.hot_key) {
                 globalShortcut.register(config.hot_key, () => {
                     if (mb.window.isFocused()) {
+                        log.debug('Toggle window: shown -> hidden');
                         mb.hideWindow();
                     } else {
+                        log.debug('Toggle window: hidden -> shown');
                         mb.showWindow();
                     }
                 });
+                log.debug('Hot key was set to:', config.hot_key);
             }
             resolve(mb);
         });
