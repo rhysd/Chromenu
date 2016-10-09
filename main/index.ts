f3f0148c836acf8f2639c797ea921d7919427551
rhysd <lin90162@yahoo.co.jp>
2016-10-10 02:18:59 +0900

    pin window and open DevTools on development

--- main/index.ts
+++ main/index.ts
@@ -12,12 +12,14 @@ function setupMenuBar(config: Config) {
     return new Promise<Menubar.MenubarApp>(resolve => {
         const html = `file://${path.join(__dirname, '..', 'renderer', 'index.html')}`;
         const icon = path.join(__dirname, '..', 'resources', `chrome-tray-icon-${config.icon_color}.png`);
+        const isDebug = process.env.NODE_ENV === 'development';
         log.debug('Will launch application:', html, icon);
         const mb = menubar({
             index: html,
             icon,
             width: 414,  // iPhone 6s
             height: 100 + 736, // Icon area height + iPhone 6s
+            alwaysOnTop: isDebug,
         });
         mb.once('ready', () => mb.showWindow());
         mb.once('after-create-window', () => {
@@ -33,6 +35,9 @@ function setupMenuBar(config: Config) {
                     }
                 });
                 log.debug('Hot key was set to:', config.hot_key);
+            }
+            if (isDebug) {
+                mb.window.webContents.openDevTools({mode: 'detach'});
             }
             resolve(mb);
         });
