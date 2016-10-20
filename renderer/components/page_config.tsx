0555f71e3292c40c0588bce891357705c323c325
rhysd <lin90162@yahoo.co.jp>
2016-10-21 01:08:46 +0900

    fix URL check

--- renderer/components/page_config.tsx
+++ renderer/components/page_config.tsx
@@ -43,7 +43,8 @@ export default class PageConfig extends React.PureComponent<PageConfigProps, {}>
     onSubmit(e: React.MouseEvent<HTMLInputElement>) {
         e.stopPropagation();
         const url = this.refs.url_input.value;
-        if (!url && !url.startsWith('http://') && !url.startsWith('https://')) {
+
+        if (!url || !url.startsWith('http://') && !url.startsWith('https://')) {
             this.refs.url_input.className = 'input is-danger';
             log.debug('Invalid URL input:', url);
             return;
