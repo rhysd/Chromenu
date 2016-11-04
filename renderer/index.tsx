3aeea9e78a90b28345fb79cf5e89e9b3aff5c298
rhysd <lin90162@yahoo.co.jp>
2016-11-05 00:11:00 +0900

    move keymap actions to keymaps.ts

--- renderer/index.tsx
+++ renderer/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {ipcRenderer as ipc, shell} from 'electron';
+import {ipcRenderer as ipc} from 'electron';
 import Store from './store';
 import App from './components/app';
 import * as Storage from './storage';
@@ -23,85 +23,7 @@ window.onunload = () => {
     Storage.save();
 };
 
-function executeJS(source: string) {
-    return () => {
-        const elem = Store.getState().webview.element;
-        if (!elem) {
-            return;
-        }
-        elem.executeJavaScript(source);
-    };
-}
-
 ipc.once('chromenu:config', (_: any, config: Config) => {
     log.debug('Config was sent from main:', config);
-    const keymaps = new Keymaps(config.keymaps);
-    for (let i = 0; i < 9; i++) {
-        keymaps.on(`page${i + 1}`, () => Store.dispatch({type: 'OpenPage', index: i}));
-    }
-    keymaps.on('reload', () => {
-        const elem = Store.getState().webview.element;
-        if (elem) {
-            elem.reload();
-        }
-    });
-    keymaps.on('back', () => {
-        const elem = Store.getState().webview.element;
-        if (elem) {
-            elem.goBack();
-        }
-    });
-    keymaps.on('forward', () => {
-        const elem = Store.getState().webview.element;
-        if (elem) {
-            elem.goForward();
-        }
-    });
-    keymaps.on('home', () => {
-        const state = Store.getState();
-        const elem = state.webview.element;
-        const current = state.pages.all.get(state.pages.index);
-        if (!current || !current.url || !elem) {
-            return;
-        }
-        elem.src = current.url;
-    });
-    keymaps.on('open-external-browser', () => {
-        const elem = Store.getState().webview.element;
-        if (!elem) {
-            return;
-        }
-        shell.openExternal(elem.src);
-    });
-    keymaps.on('next-page', () => {
-        const index = Store.getState().pages.index;
-        if (index === null) {
-            return;
-        }
-        Store.dispatch({type: 'OpenPage', index: index + 1});
-    });
-    keymaps.on('previous-page', () => {
-        const index = Store.getState().pages.index;
-        if (index === null) {
-            return;
-        }
-        Store.dispatch({type: 'OpenPage', index: index - 1});
-    });
-    keymaps.on('scroll-down', executeJS('window.scrollBy(0, window.innerHeight / 5)'));
-    keymaps.on('scroll-up', executeJS('window.scrollBy(0, -window.innerHeight / 5)'));
-    keymaps.on('scroll-left', executeJS('window.scrollBy(-window.innerWidth / 3, 0)'));
-    keymaps.on('scroll-right', executeJS('window.scrollBy(window.innerWidth / 3, 0)'));
-    keymaps.on('scroll-down-half-page', executeJS('window.scrollBy(0, window.innerHeight / 2)'));
-    keymaps.on('scroll-up-half-page', executeJS('window.scrollBy(0, -window.innerHeight / 2)'));
-    keymaps.on('scroll-down-page', executeJS('window.scrollBy(0, window.innerHeight)'));
-    keymaps.on('scroll-up-page', executeJS('window.scrollBy(0, -window.innerHeight)'));
-    keymaps.on('scroll-bottom', executeJS('window.scrollTo(0, document.body.scrollHeight)'));
-    keymaps.on('scroll-top', executeJS('window.scrollTo(0, 0)'));
-    keymaps.on('open-devtools', () => {
-        const elem = Store.getState().webview.element;
-        if (!elem) {
-            return;
-        }
-        elem.getWebContents().openDevTools({mode: 'detach'});
-    });
+    new Keymaps(config.keymaps);
 });
--- renderer/keymaps.ts
+++ renderer/keymaps.ts
@@ -1,6 +1,18 @@
 import {EventEmitter} from 'events';
 import * as Mousetrap from 'mousetrap';
+import {shell} from 'electron';
+import Store from './store';
 import log from './log';
+
+function executeJS(source: string) {
+    return () => {
+        const elem = Store.getState().webview.element;
+        if (!elem) {
+            return;
+        }
+        elem.executeJavaScript(source);
+    };
+}
 
 export default class Keymaps extends EventEmitter {
     constructor(public config: {[key: string]: KeymapAction}) {
@@ -13,5 +25,72 @@ export default class Keymaps extends EventEmitter {
                 this.emit(this.config[key]);
             });
         }
+        this.registerAllKeymaps();
+    }
+
+    registerAllKeymaps() {
+        for (let i = 0; i < 9; i++) {
+            this.on(`page${i + 1}`, () => Store.dispatch({type: 'OpenPage', index: i}));
+        }
+        this.on('reload', () => {
+            const elem = Store.getState().webview.element;
+            if (elem) {
+                elem.reload();
+            }
+        });
+        this.on('back', () => {
+            const elem = Store.getState().webview.element;
+            if (elem) {
+                elem.goBack();
+            }
+        });
+        this.on('forward', () => {
+            const elem = Store.getState().webview.element;
+            if (elem) {
+                elem.goForward();
+            }
+        });
+        this.on('home', () => {
+            const state = Store.getState();
+            const elem = state.webview.element;
+            const current = state.pages.all.get(state.pages.index);
+            if (current && current.url && elem) {
+                elem.src = current.url;
+            }
+        });
+        this.on('open-external-browser', () => {
+            const elem = Store.getState().webview.element;
+            if (elem) {
+                shell.openExternal(elem.src);
+            }
+        });
+        this.on('next-page', () => {
+            const index = Store.getState().pages.index;
+            if (index !== null) {
+                Store.dispatch({type: 'OpenPage', index: index + 1});
+            }
+        });
+        this.on('previous-page', () => {
+            const index = Store.getState().pages.index;
+            if (index !== null) {
+                Store.dispatch({type: 'OpenPage', index: index - 1});
+            }
+        });
+        this.on('scroll-down', executeJS('window.scrollBy(0, window.innerHeight / 5)'));
+        this.on('scroll-up', executeJS('window.scrollBy(0, -window.innerHeight / 5)'));
+        this.on('scroll-left', executeJS('window.scrollBy(-window.innerWidth / 3, 0)'));
+        this.on('scroll-right', executeJS('window.scrollBy(window.innerWidth / 3, 0)'));
+        this.on('scroll-down-half-page', executeJS('window.scrollBy(0, window.innerHeight / 2)'));
+        this.on('scroll-up-half-page', executeJS('window.scrollBy(0, -window.innerHeight / 2)'));
+        this.on('scroll-down-page', executeJS('window.scrollBy(0, window.innerHeight)'));
+        this.on('scroll-up-page', executeJS('window.scrollBy(0, -window.innerHeight)'));
+        this.on('scroll-bottom', executeJS('window.scrollTo(0, document.body.scrollHeight)'));
+        this.on('scroll-top', executeJS('window.scrollTo(0, 0)'));
+        this.on('open-devtools', () => {
+            const elem = Store.getState().webview.element;
+            if (elem) {
+                elem.getWebContents().openDevTools({mode: 'detach'});
+            }
+        });
     }
 }
