01e44225badfa289bc92f66297c25b053b8cc9ff
rhysd <lin90162@yahoo.co.jp>
2016-11-06 00:12:45 +0900

    support user.css

--- renderer/index.tsx
+++ renderer/index.tsx
@@ -1,7 +1,9 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {ipcRenderer as ipc} from 'electron';
+import {ipcRenderer as ipc, remote} from 'electron';
 import Store from './store';
 import App from './components/app';
 import * as Storage from './storage';
@@ -27,4 +29,24 @@ ipc.once('chromenu:config', (_: any, config: Config) => {
     log.debug('Config was sent from main:', config);
     const keymaps = new Keymaps(config.keymaps);
     keymaps.registerAllKeymaps();
+});
+
+const UserCss = path.join(remote.app.getPath('userData'), 'user.css');
+
+fs.stat(UserCss, (err, stats) => {
+    if (err) {
+        // No user.css found. Skipping.
+        log.debug('No user.css found. Skipping.');
+        return;
+    }
+
+    if (!stats.isFile()) {
+        log.error('Invalid user.css. It is not a file!');
+        return;
+    }
+
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = UserCss;
+    document.head.appendChild(link);
 });
